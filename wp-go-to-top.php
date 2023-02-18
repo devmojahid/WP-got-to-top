8c45184096c959e4e2a27bba099adee2b7d7f188
mojahid-islam <raofahmedmojahid@gmail.com>
2023-02-18 19:48:32 +0600

    first commit

---
+++ wp-go-to-top.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* Plugin Name: WP Go To Top
+* Plugin URI: https://wordpress.org/plugins/wp-go-to-top/
+* Description: This plugin adds a go to top button to your Wordpress website.
+* Version: 1.0.0
+* Requires at least: 5.2
+* Requires PHP: 7.2
+* Author: Mojahid Islam
+* Author URI: http://devmojahid.me/
+* License: GPL v2 or later
+* License URI: https://www.gnu.org/licenses/gpl-2.0.html
+* Update URI: https://github.com/mojahid7
+* Text Domain: wgtt
+*/
+
+
+
+?>
